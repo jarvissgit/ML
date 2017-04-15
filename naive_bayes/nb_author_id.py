49ced0eaa73d378c91d644c79d55242956840e17
J P <km4gck@gmail.com>
2017-04-15 20:37:19 +0530

    added code for training time and prediction time

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -29,12 +29,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
+t0 = time()
 clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 pred = clf.predict(features_test)
-print pred
+
+#print pred
 
 from sklearn.metrics import accuracy_score
+t1 = time()
 accuracy = accuracy_score(pred, labels_test)
+print "prediction time:", round(time()-t1, 3), "s"
 print accuracy
 #########################################################
 
